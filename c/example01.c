8f771deef61b06e562892b8163ed6fc666f60ed8
octavegl <octave.gassot-laffont@outscale.com>
2021-05-06 16:22:29 +0000

    Simplified malloc + memcpy

--- c/example01.c
+++ c/example01.c
@@ -12,12 +12,12 @@ This example also shows how to read available images and all their informations.
 
 // Function that will write the data inside a variable
 size_t write_data(void *data, size_t size, size_t nmemb, void *userp){
-  size_t realsize = size * nmemb;
+  size_t realsize = size * nmemb * 2;
 
   char **response = (char **) userp;
   
-  char *temp = (char*) malloc(realsize);
-  *response  = (char *)  memcpy(temp, data, realsize); 
+  *response = malloc(realsize);
+  memcpy(*response, data, realsize); 
   
   return realsize; 
 }
