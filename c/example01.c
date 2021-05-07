c1369e4a4ad7ef4d26f16030ba0b3c07a679f2f4
octavegl <octave.gassot-laffont@outscale.com>
2021-05-07 09:36:12 +0000

    Adapting to official env variable

--- c/example01.c
+++ c/example01.c
@@ -10,7 +10,10 @@ This example also shows how to read available images and all their informations.
 #include <stdlib.h>
 #include "curl/curl.h"
 
-// Function that will write the data inside a variable
+#define AK_SIZE 20
+#define SK_SIZE 40
+
+/* Function that will write the data inside a variable */
 size_t write_data(void *data, size_t size, size_t nmemb, void *userp){
   size_t realsize = size * nmemb * 2;
 
@@ -23,31 +26,46 @@ size_t write_data(void *data, size_t size, size_t nmemb, void *userp){
 }
 
 int main(void){
-  // Getting the access key / secret key from environement
-  const char *ak_sk = getenv("AKSK");
+  /* Getting the access key / secret key from environement */
+  const char *ak = getenv("OSC_ACCESS_KEY");
+  const char *sk = getenv("OSC_SECRET_KEY");
 
-  // Variable that will store the data
+  char ak_sk[AK_SIZE + SK_SIZE + 2];
+
+  printf("%s %s %i %i", ak, sk, AK_SIZE, SK_SIZE);
+
+  
+  if (strlen(ak) != AK_SIZE || strlen(sk) != SK_SIZE) {
+    abort();
+  }
+ 
+  
+  stpcpy(stpcpy(stpcpy(ak_sk, ak), ":"), sk);
+
+  printf("%s", ak_sk);
+
+  /* Variable that will store the data */
   char *response;
   
   CURLcode res;
 
-  // Creating the handler
+  /* Creating the handler */
   CURL *c = curl_easy_init();
 
-  // Setting the url
+  /* Setting the url */
   curl_easy_setopt(c, CURLOPT_URL, "https://api.eu-west-2.outscale.com/api/v1/ReadImages");
 
-  // Let's see what curl is doing
+  /* Let's see what curl is doing */
   curl_easy_setopt(c, CURLOPT_VERBOSE, 1L);
 
-  // Empty post field to indicate we want to send a post request
+  /* Empty post field to indicate we want to send a post request */
   curl_easy_setopt(c, CURLOPT_POSTFIELDS, "");
 
-  // Telling curl to use the write_data function to write over response
+  /* Telling curl to use the write_data function to write over response */
   curl_easy_setopt(c, CURLOPT_WRITEFUNCTION, write_data);
   curl_easy_setopt(c, CURLOPT_WRITEDATA, &response);
 
-  // For authentification we specify the method and our acces key / secret key
+  /* For authentification we specify the method and our acces key / secret key */
   curl_easy_setopt(c, CURLOPT_AWS_SIGV4, "osc");
   curl_easy_setopt(c, CURLOPT_USERPWD, ak_sk);
 
