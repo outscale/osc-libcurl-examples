4da4d4b428020379e0e49cd71182ec9e8962766a
octavegl <octave.gassot-laffont@outscale.com>
2021-05-07 08:08:36 +0000

    Fix typo

--- c/example01.c
+++ c/example01.c
@@ -14,7 +14,7 @@ This example also shows how to read available images and all their informations.
 size_t write_data(void *data, size_t size, size_t nmemb, void *userp){
   size_t realsize = size * nmemb * 2;
 
-  char **response = (char **) userp;
+  char **response = userp;
   
   *response = malloc(realsize);
   memcpy(*response, data, realsize); 
