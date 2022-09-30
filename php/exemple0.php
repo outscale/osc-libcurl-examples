31bba84d2b1a9d6aa03ba1bc423e0f0715b2fbe0
Matthias Gatto <matthias.gatto@outscale.com>
2022-09-30 14:02:24 +0200

    more documentation, for your favorit language
    not mine, I prefer C.
    
    Signed-off-by: Matthias Gatto <matthias.gatto@outscale.com>

--- php/exemple0.php
+++ php/exemple0.php
@@ -3,6 +3,7 @@
 //
 // REQUIRE PHP 8.2 OR SUPERRIOR
 // This message been writed in 2022. Most likelly you need to recompille it.
+// it's like ./buildroor; ./configure --with-curl; make -jNUMBER_OF_CORE_FLEX; ./sapi/cli/php PATH/TO/THIS/FILE
 //
 
 define("AK_SIZE", 20);
